9c38e19da2fe8c999c570bfaf3ceeb02ac482eaa
Kent Allen Duke <kentallenduke@gmail.com>
2023-10-22 21:43:14 -0700

    Quick prototype of the Madlibs project.

--- madlibs/script.js
+++ madlibs/script.js
@@ -1,5 +1,27 @@
 (function(){
+    var wordElements = document.querySelectorAll(".word");
 
-    
+    wordElements.forEach(function(element) {
+        element.addEventListener('blur', function() {
+            var text = this.value;
+            var myData = this.getAttribute("data-word");
+            var matchingElements = document.querySelectorAll(".word[data-word='" + myData + "']");
+            
+            matchingElements.forEach(function(matchingElement) {
+                matchingElement.value = text;
+            });
+        });
+    });
     
 })();
+
+
+// $(".word").on('blur', function(){
+//     var text = $(this).val();
+//     var myData = $(this).data("word");
+//     $(".word[data-word="+myData+"]").val( text );
+// });
+
+$("#flip").click(function(){
+    $("#cardarea").toggleClass("flipped");
+});
